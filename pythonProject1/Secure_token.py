15567b92c89dfd5d0768fb51aab9f5908891aeca
Andrzej <asidor23@gmail>
2024-09-13 01:13:15 +0200

    Commit

---
+++ pythonProject1/Secure_token.py
@@ -0,0 +1,19 @@
+import secrets
+#Genereate a secure random token
+
+token = secrets.token_hex(16)
+print(f"Secure Token: {token}")
+
+#Genereate a secure random url
+url_safe_token = secrets.token_urlsafe(16)
+print(f"Secure url: {url_safe_token} ")
+
+#Genereate a secure random int
+random_int = secrets.randbelow(200)+1
+print(f"Secure Random Number: {random_int}")
+
+#Genereate a secure random list
+itens= ['banan','wisna']
+random_inems = secrets.choice(itens)
+print(f"Losowy obiekt {random_inems}")
+
